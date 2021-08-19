ade10a86d6f422b13e9b3a621279044652976ec2
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-08-19 18:13:18 +0300

    Adding babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HTMLPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: ['@babel/polyfill','./src/index.js'],
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js'
@@ -19,5 +19,19 @@ module.exports = {
     ],
     resolve: {
         extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+                }
+            }
+        ]
     }
 }
