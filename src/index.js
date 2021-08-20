8be46f47f19cd8f02c4e3c6834662c3db3e35dab
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-08-20 12:50:27 +0300

    Binding navigation to pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import { HeaderComponent } from './components/header.component';
 import {NavigationComponent} from './components/navigation.component';
+import {CreateComponent} from './components/create.component';
+import {PostsComponent} from './components/posts.component';
+import {FavoriteComponent} from './components/favorite.component';
 
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+
+const create = new CreateComponent('create')
+const posts = new PostsComponent('posts')
+const favorite = new FavoriteComponent('favorite')
+
+
+navigation.registerTabs ([
+    {name: 'create', component: create},
+    {name: 'posts', component: posts},
+    {name: 'favorite', component: favorite}
+])
