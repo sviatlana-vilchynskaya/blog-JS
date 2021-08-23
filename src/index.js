f5247cd6f4832b1fa5efa8cae9ba381561e06840
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-08-23 21:05:31 +0300

    Displaying a list of favorite posts

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const loader = new LoaderComponent('loader')
 
 const create = new CreateComponent('create')
 const posts = new PostsComponent('posts', {loader})
-const favorite = new FavoriteComponent('favorite')
+const favorite = new FavoriteComponent('favorite', {loader})
 
 
 navigation.registerTabs ([
