62bd8eb9601a8a4507674d4052299495261c95cf
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-08-23 17:47:12 +0300

    Add / remove favorites function from LocalStorage

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,17 +1,23 @@
 import { Component } from '../core/component';
 import { apiService } from '../services/api.service';
 import { TransformService } from '../services/transform.service';
+import { renderPost } from '../templates/post.template'
 
 export class PostsComponent extends Component {
     constructor(id, {loader}) {
         super(id)
         this.loader = loader
     }
+
+    init() {
+        this.$el.addEventListener('click', buttonHandler.bind(this))
+    }
+
     async onShow() {
         this.loader.show()
         const fbData = await apiService.fetchPosts()
         const posts = TransformService.fbObjectToArray(fbData)
-        const html = posts.map(post => renderPost(post))
+        const html = posts.map(post => renderPost(post, {withButton: true}))
         this.loader.hide()
         this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
     }
@@ -21,27 +27,26 @@ export class PostsComponent extends Component {
     }
 }
 
-function renderPost(post) {
-    const tag = post.type === 'news'
-        ? '<li class="tag tag-blue tag-rounded">Новость</li>'
-        : '<li class="tag tag-rounded">Заметка</li>'
-    const button = '<button class="button-round button-small button-primary">Сохранить</button>'
+function buttonHandler(event) {
+    const $el = event.target
+    const id = $el.dataset.id
 
-    return `
-    <div class="panel">
-        <div class="panel-head">
-            <p class="panel-title">${post.title}</p>
-            <ul class="tags">
-              ${tag}
-            </ul>
-        </div>
-        <div class="panel-body">
-            <p class="multi-line">${post.fulltext}</p>
-        </div>
-        <div class="panel-footer w-panel-footer">
-            <small>${post.date}</small>
-            ${button}
-        </div>
-    </div>
-    `
+    if(id) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || []
+
+        if (favorites.includes(id)) {
+            $el.textContent = 'Сохранить'
+            $el.classList.add('button-primary')
+            $el.classList.remove('button-danger')
+            favorites = favorites.filter(fId => fId !== id)
+        } else {
+            $el.textContent = 'Удалить'
+            $el.classList.add('button-danger')
+            $el.classList.remove('button-primary')
+            favorites.push(id)
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }
+
 }
